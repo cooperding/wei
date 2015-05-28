6be37342388a896178991d22f786e5a87c175d7a
dingcms <qiuyuncode@163.com>
2015-05-28 21:20:51 +0800

    删除代码

--- Application/Home/Logic/UserLogic.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-//D('User','Logic') //实例化UserLogic
-namespace Home\Logic;
-
-use Think\Model;
-
-class UserLogic extends Model {
-    
-}
--- Application/User/Service/UserService.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-//D('User','Service') //实例化UserService
-namespace Home\Service;
-
-use Think\Model;
-
-class UserService extends Model {
-    
-}
